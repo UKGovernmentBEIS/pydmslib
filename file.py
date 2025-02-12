67037892477df367a72a0a5fab5ebfd9285ddd71
BenNBEIS <116082217+BenNBEIS@users.noreply.github.com>
2025-02-12 10:40:18 +0000

    changed over-specific variable
    base-dir -> dir. This method doesn't always have to reference base

--- file.py
+++ file.py
@@ -5,15 +5,15 @@ from pyspark.dbutils import DBUtils
 spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
 
-def read_parquet_files_and_add_input_file(base_dir, pattern=""):
+def read_parquet_files_and_add_input_file(dir, pattern=""):
     """
     Reads Parquet files from the specified directory and adds a new column indicating the source file name.
     Args:
-        base_dir (str): The base directory where the Parquet files are stored.
+        dir (str): The directory where the Parquet files are stored. For reading pyspark dataframes, you must include the LAKE_ROOT_SPARK variable in the dir variable.
         pattern (str, optional): An optional pattern to filter specific files. Defaults to an empty string.
     Returns:
         DataFrame: A PySpark DataFrame with an additional column 'inputFile' containing the source file name.
     """
-    file_path = base_dir + pattern
+    file_path = dir + pattern
     df = spark.read.parquet(file_path)
     return df.withColumn("inputFile", f.col("_metadata.file_name"))
