a37510db2e9e1729da84a129c2081c2cc450387c
BenNBEIS <116082217+BenNBEIS@users.noreply.github.com>
2025-02-14 10:44:09 +0000

    remove unused dbutils, add docstring, changed dir -> directory to stop overwriting a variable

--- pydmslib/file.py
+++ pydmslib/file.py
@@ -1,12 +1,10 @@
-import os
+""" file.py contains methods that help with standardising reading/cleaning steps """
 import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
-from pyspark.dbutils import DBUtils
 
 spark = SparkSession.builder.getOrCreate()
-dbutils = DBUtils(spark)
 
-def read_parquet_files_and_add_input_file(dir: str, pattern: str = ""):
+def read_parquet_files_and_add_input_file(directory: str, pattern: str = ""):
     """
     Reads Parquet files from the specified directory and adds a new column indicating the source file name.
     Args:
@@ -16,6 +14,6 @@ def read_parquet_files_and_add_input_file(dir: str, pattern: str = ""):
         DataFrame: A PySpark DataFrame with an additional column 'inputFile' containing the source file name.
     """
 
-    file_path = dir + pattern
+    file_path = directory + pattern
     df = spark.read.parquet(file_path)
     return df.withColumn("inputFile", f.col("_metadata.file_name"))
